f340c27d1843f729037c87b54224e59cf119fcfa
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-09-09 14:21:40 -0300

    Implementação da Infra API Gateway

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -41,18 +41,32 @@ resource "aws_api_gateway_integration" "mixfast_api_gateway_integration" {
   http_method             = aws_api_gateway_method.mixfast_api_gateway_method.http_method
   integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
-  uri                     = "http://${var.name}.com/{proxy}"
+  uri                     = "http://mixfast.com/{proxy}"
 
   request_parameters =  {
     "integration.request.path.proxy" = "method.request.path.proxy"
   }
 }
 
-#resource "aws_api_gateway_domain_name" "mixfast_api_gateway_domain" {
-#  domain_name             = "${var.name}.com"
-#}
-#
-#resource "aws_api_gateway_base_path_mapping" "mixfast_api_gateway_base_path_mapping" {
-#  api_id      = aws_api_gateway_rest_api.mixfast_api_gateway.id
-#  domain_name = aws_api_gateway_domain_name.mixfast_api_gateway_domain.domain_name
-#}
+resource "aws_lambda_function" "mixfast_lambda_authorizer" {
+  function_name = "mixfast_lambda_authorizer"
+  runtime       = "nodejs18.x"
+  handler       = "index.mjs"
+  role          = "arn:aws:iam::022874923015:role/mixfast-lambda-role"
+  filename      = "mixfast_lambda.zip"
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [var.security_group]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_api_gateway_authorizer" "mixfast_api_gateway_authorizer" {
+  name                   = "${var.name}_authorizer"
+  rest_api_id            = aws_api_gateway_rest_api.mixfast_api_gateway.id
+  authorizer_uri         = aws_lambda_function.mixfast_lambda_authorizer.invoke_arn
+  authorizer_credentials = "arn:aws:iam::022874923015:role/mixfast-lambda-role"
+  type                   = "TOKEN"
+}
