09b8e13c74a7b41fdf8442e27f2a85a5bd1cdcf5
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-09-10 23:32:03 -0300

    Implementação da Infra API Gateway

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -14,25 +14,27 @@ provider "aws" {
 resource "aws_api_gateway_rest_api" "mixfast_api_gateway" {
   name        = "${var.name}-api-gateway"
   description = "API Gateway do Mix Fast"
+  body        = file("swagger_mixfast.json")
 
   tags = var.tags
 }
 
-resource "aws_api_gateway_resource" "mixfast_api_gateway_resource" {
+resource "aws_api_gateway_deployment" "mixfast_api_gateway_deployment" {
   rest_api_id = aws_api_gateway_rest_api.mixfast_api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.mixfast_api_gateway.root_resource_id
-  path_part   = "{proxy+}"
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.mixfast_api_gateway.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_api_gateway_method" "mixfast_api_gateway_method" {
+resource "aws_api_gateway_stage" "mixfast_api_gateway_stage" {
+  deployment_id = aws_api_gateway_deployment.mixfast_api_gateway_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.mixfast_api_gateway.id
-  resource_id   = aws_api_gateway_resource.mixfast_api_gateway_resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
-
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
+  stage_name    = "mixfast_dev"
 }
 
 resource "aws_lambda_function" "mixfast_lambda_authorizer" {
