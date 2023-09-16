50038e4cf558b532f75081651404c7b79a539fb2
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-09-15 22:08:10 -0300

    Implementação da Infra API Gateway

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -61,11 +61,18 @@ resource "aws_api_gateway_integration" "mixfast_api_gateway_integration_vpc_link
   connection_id   = "wss4wt"
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
+resource "aws_api_gateway_method_response" "mixfast_api_gateway_method_response" {
   rest_api_id = aws_api_gateway_rest_api.mixfast_api_gateway.id
   resource_id = aws_api_gateway_resource.mixfast_api_gateway_resource.id
   http_method = aws_api_gateway_method.mixfast_api_gateway_method.http_method
   status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "mixfast_api_gateway_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.mixfast_api_gateway.id
+  resource_id = aws_api_gateway_resource.mixfast_api_gateway_resource.id
+  http_method = aws_api_gateway_method.mixfast_api_gateway_method.http_method
+  status_code = aws_api_gateway_method_response.mixfast_api_gateway_method_response.status_code
 }
 
 resource "aws_api_gateway_deployment" "mixfast_api_gateway_deployment" {
