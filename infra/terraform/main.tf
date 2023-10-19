b50ffcff23ef57922c57a490e766a8be4c22be04
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-18 22:45:19 -0300

    Implementação da Infra API Gateway

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -40,7 +40,7 @@ resource "aws_api_gateway_integration" "mixfast_api_gateway_integration_vpc_link
   resource_id = aws_api_gateway_resource.mixfast_api_gateway_resource.id
   http_method = aws_api_gateway_method.mixfast_api_gateway_method.http_method
 
-  type                    = "HTTP"
+  type                    = "HTTP_PROXY"
   uri                     = "http://mixfast-nlb-ebefb32cbfc58f9c.elb.us-east-2.amazonaws.com:9080/{id}"
   integration_http_method = "ANY"
   passthrough_behavior    = "WHEN_NO_MATCH"
@@ -103,4 +103,32 @@ resource "aws_api_gateway_method_settings" "mixfast_api_gateway_settings" {
     data_trace_enabled = true
     metrics_enabled = true
   }
+}
+
+resource "aws_api_gateway_rest_api_policy" "api_gateway_policy" {
+  rest_api_id = aws_api_gateway_rest_api.mixfast_api_gateway.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "execute-api:Invoke",
+        "Resource": [
+          "execute-api:/*"
+        ]
+      },
+      {
+        "Effect": "Deny",
+        "Principal": "*",
+        "Action": "execute-api:Invoke",
+        "Resource": [
+          "execute-api:/*"
+        ]
+      }
+    ]
+  }
+EOF
 }
