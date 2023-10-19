2b0d90c64d388a13897e819f59a3b640d0410d37
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-19 00:59:41 -0300

    Implementação da Infra API Gateway

--- infra/terraform/aws-iam-role-policy.tf
+++ infra/terraform/aws-iam-role-policy.tf
@@ -15,6 +15,32 @@ resource "aws_iam_role" "api_gateway_cloudwatch_role" {
   tags = var.tags
 }
 
+resource "aws_iam_policy" "api_gateway_cloudwatch_policy" {
+  name = "${var.name}_api_gateway_cloudwatch_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents",
+        "logs:GetLogEvents",
+        "logs:FilterLogEvents"
+      ]
+      Resource = ["*"]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch_attachment" {
+  policy_arn = aws_iam_policy.api_gateway_cloudwatch_policy.arn
+  role = aws_iam_role.api_gateway_cloudwatch_role.name
+}
+
 data "aws_iam_policy_document" "invocation_role" {
   statement {
     effect = "Allow"
@@ -42,7 +68,7 @@ data "aws_iam_policy_document" "invocation_policy" {
   }
 }
 
-resource "aws_iam_role_policy" "api_gateway_lambda_policy" {
+resource "aws_iam_role_policy" "api_gateway_lambda_role_policy" {
   name   = "${var.name}_api_gateway_lambda_policy"
   role   = aws_iam_role.api_gateway_lambda_role.id
   policy = data.aws_iam_policy_document.invocation_policy.json
