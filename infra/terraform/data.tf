e5fbdca0453a3e6291c930d044503096e1b1fc49
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-19 20:23:18 -0300

    Implementação da Infra API Gateway

--- infra/terraform/data.tf
+++ infra/terraform/data.tf
@@ -7,6 +7,5 @@ data "template_file" "mixfast_contrato_template" {
     port        = 9080
     auth        = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:022874923015:function:mixfast_lambda_authorizer/invocations"
     credentials = aws_iam_role.api_gateway_lambda_role.arn
-    api_key     = aws_api_gateway_rest_api.mixfast_api_gateway.api_key_source
   }
 }
